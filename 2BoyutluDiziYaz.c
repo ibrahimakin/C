52881d32c9163c52cfafb904a98a09a2b4141348
ibrahim <ibrahim-akin@msn.com>
2017-12-08 20:14:51 +0300

    Add files via upload
    Fonksiyona adres göndererek 2 boyutlu dizi yazdırır.

---
+++ 2BoyutluDiziYaz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int B[4][5]={{4564,16,5,42,3},{64,55,66,76,8},{454,165,5,2,3},{454,16,5,26,3}};
+
+    diziF(&B[0],4);
+
+    printf("\n");
+    return 0;
+}
+
+void diziF(int (*dizi)[5],int size){
+    int i,j;
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+       // printf("%4d\n",dizi[i][j]);
+        printf("%4d",*(*(dizi+i)+j));
+        }
+    printf("\n");
+    }
+    system("PAUSE");
+}
