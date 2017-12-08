03c6928c3f6149d6070575a3b74f237cf125dfe6
ibrahim <ibrahim-akin@msn.com>
2017-12-08 19:56:54 +0300

    Add files via upload
    Dinamik dizi oluşturup yazıdırır.

---
+++ DinamikDizi.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void dinamik_dizi_olustur(int **dizi, int eleman_sayisi){
+    int i;
+    *dizi = (int *)malloc(eleman_sayisi*sizeof(int));
+    for(i=0;i<eleman_sayisi;i++){
+        *(*dizi+i)= rand()%99;
+        }
+}
+
+void tek_boyutlu_dizi_yaz(int *dizi, int eleman_sayisi){
+    int i;
+
+    for(i=0;i<eleman_sayisi;i++){
+        printf("0x%p ", (dizi+i));
+    }
+
+    printf("\n");
+    for(i=0;i<eleman_sayisi;i++){
+        printf("%9d  ", *(dizi+i));
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int **A, **B, **C;
+    int *dizi;
+    int eleman_sayisi;
+    srand(time(NULL));
+    eleman_sayisi = 10;
+    dinamik_dizi_olustur(&dizi, eleman_sayisi);
+    tek_boyutlu_dizi_yaz(dizi, eleman_sayisi);
+    system("PAUSE");
+    return 0;
+}
