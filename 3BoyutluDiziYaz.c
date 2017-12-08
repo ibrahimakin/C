35ef9bbd0aab9159e103d0259e861065e55277d3
ibrahim <ibrahim-akin@msn.com>
2017-12-08 19:43:16 +0300

    Update 3BoyutluDiziYaz.c

--- 3BoyutluDiziYaz.c
+++ 3BoyutluDiziYaz.c
@@ -15,7 +15,6 @@ void yazdir(int (*dizi)[4][3],int size) {
 int main() {
     int dizi[2][4][3]={{{1,2,3},{2,3,4},{3,4,5},{4,5,6}},{{5,6,7},{6,7,8},{7,8,9},{8,9,0}}};
     yazdir(dizi,2);
+    system("PAUSE");
     return 0;
 }
-
-
